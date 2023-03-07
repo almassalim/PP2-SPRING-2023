7a3f34958afc7661a63a177e1e747dae859be84c
almassalim <123352943+almassalim@users.noreply.github.com>
2023-03-07 14:01:28 +0600

    Delete lab6/builtin-functions directory

--- lab6/builtin-functions/1.py
+++
@@ -1,3 +0,0 @@
-import math
-somelist = map(int, input('Input a list through comma: ').split(','))
-print(math.prod(somelist))
--- lab6/builtin-functions/3.py
+++
@@ -1,2 +0,0 @@
-somestr = input('Input a string: ')
-print(somestr.lower().replace(' ', '') == ''.join(reversed(somestr.lower().replace(' ', ''))) and 'Palindrome' or 'Not palindrome')
